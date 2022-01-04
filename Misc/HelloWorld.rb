35d91c9255cb04525d76e37a88fd32019f20ac72
Edward Rene Acosta <Eacosta2@satx.rr.com>
2022-01-03 22:00:01 -0600

    ran bundle install due to error

---
+++ Misc/HelloWorld.rb
@@ -0,0 +1,12 @@
+puts "Hello World using string"
+
+greetings = "Howdy Parder using variable"
+
+puts greetings
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello "Hey There using a method (function)"
+
